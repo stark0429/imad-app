9107f46044f5bebcd7ee9fb625fc46f1e1989a55
stark0429 <dileepvirupakshi2904@gmail.com>
2017-08-06 09:55:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text')
 var img =document.getElementById('madi')
+var margLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function () {
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,50);
 };
